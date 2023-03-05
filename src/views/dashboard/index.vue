d8f43318e6e5cd9bf4fa56e5a0fee7e6d5ea3fb2
dong_960010 <dong_960010@163.com>
2023-03-05 08:50:04 +0800

    chore: 初始化

---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="dashboard-container">
+    <div class="dashboard-text">name: {{ name }}</div>
+    <section class="icon-list">
+      <i class="nes-mario" />
+      <i class="nes-ash" />
+      <i class="nes-pokeball" />
+      <i class="nes-bulbasaur" />
+      <i class="nes-charmander" />
+      <i class="nes-squirtle" />
+      <i class="nes-kirby" />
+    </section>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'Dashboard',
+  computed: {
+    ...mapGetters([
+      'name'
+    ])
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 30px;
+  }
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+</style>
